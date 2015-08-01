73a998893294a2d709619b11eb4427cb435f5525
Aires <airesvsg@gmail.com>
2015-08-01 12:09:53 -0300

    removing unnecessary code

--- includes/class-acf-to-wp-rest-api-options.php
+++ includes/class-acf-to-wp-rest-api-options.php
@@ -7,10 +7,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 if ( ! class_exists( 'ACF_To_WP_REST_API_Options' ) ) {
 	class ACF_To_WP_REST_API_Options extends ACF_To_WP_REST_API_Base {
 
-		public function __construct() {
-			parent::__construct();
-		}
-
 		public function register_routes( $routes ) {
 			$routes['/acf/options'] = array( 
 				array( array( __CLASS__, 'get_options' ), WP_JSON_Server::READABLE ),
