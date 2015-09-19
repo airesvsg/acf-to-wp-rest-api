2efdb9ce74e4bafb9c7177f832d5e3395bf3e4a8
Aires <airesvsg@gmail.com>
2015-09-19 00:05:34 -0300

    Updating the tested version

--- acf-to-wp-rest-api.php
+++ acf-to-wp-rest-api.php
@@ -4,7 +4,7 @@
  * Description: Get all ACF fields in WP REST API responses.
  * Author: Aires Gonçalves
  * Author URI: airesvsg.github.com
- * Version: 0.0.2
+ * Version: 1.0.0
  * Plugin URI: airesvsg.github.com/acf-to-wp-rest-api
  */
 
