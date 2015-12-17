90f6f23ad7cdadab9faabc3f06698bfac8988a31
Aires <airesvsg@gmail.com>
2015-12-17 15:20:01 -0200

    improve filter for cpt

--- includes/class-acf-to-wp-rest-api-custom-post-type.php
+++ includes/class-acf-to-wp-rest-api-custom-post-type.php
@@ -9,20 +9,21 @@ if ( ! class_exists( 'ACF_To_WP_REST_API_Custom_Post_Type' ) ) {
 
 		public function __construct() {
 			parent::__construct();
-
+			
 			$default    = array( 'post', 'page', 'attachment' );
-
-			$args       = apply_filters( 'acf_to_wp_rest_api_cutom_post_type_args', array( 'public' => true ) );
+			$args       = apply_filters( 'acf_to_wp_rest_api_cutom_post_type_args', array( 'public' => true ) );	
 			$post_types = get_post_types( $args );	
+			
+			if ( ! is_array( $post_types ) ) {
+				$post_types = array();
+			}
 
-			if ( is_array( $post_types ) ) {
-				$post_types = apply_filters( 'acf_to_wp_rest_api_cutom_post_types', array_diff( $post_types, $default ) );
-				
-				if ( is_array( $post_types ) && count( $post_types ) > 0 ) {
-					foreach ( $post_types as $pt ) {
-						add_filter( "rest_prepare_{$pt}", array( $this, 'get_fields' ), 10, 3 );
-					}
-				}				
+			$post_types = apply_filters( 'acf_to_wp_rest_api_cutom_post_types', array_diff( $post_types, $default ) );
+			
+			if ( is_array( $post_types ) && count( $post_types ) > 0 ) {
+				foreach ( $post_types as $pt ) {
+					add_filter( "rest_prepare_{$pt}", array( $this, 'get_fields' ), 10, 3 );
+				}
 			}
 		}
 
